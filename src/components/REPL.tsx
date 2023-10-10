b39d60f4ec748aacd6a899624360ce8c947c4d5f
Tiffany Huang <goldentogawhale1258@gmail.com>
2023-10-10 12:39:24 -0400

    feat: repl history brief/verbose starting

--- src/components/REPL.tsx
+++ src/components/REPL.tsx
@@ -16,32 +16,62 @@ import mockedJson from "../modules/mockedJson";
 export default function REPL() {
   // Add some kind of shared state that holds all the commands submitted.
   //const submittedCommands: string[] = [];
-  const [submittedCommands, setSubmittedCommands] = useState<string[]>([]);
+  const [submittedCommands, setSubmittedCommands] = useState<JSX.Element[]>([]);
+  const [submittedInputs, setSubmittedInputs] = useState<string[]>([]);
+  const [mode, setMode] = useState<string>("Brief");
   const file1 = mockedJson().file1;
 
   function addCommand(str: string) {
     const newCommand = parseCommandLine(str);
+    setSubmittedInputs([...submittedInputs, str]);
     setSubmittedCommands([...submittedCommands, newCommand]);
   }
 
   const parseCommandLine = (str: string) => {
-    let newLine = "";
+    const newCommands: JSX.Element[] = [];
     if (str.includes("load_file")) {
+      const body: string[] = [];
       file1.data.body.forEach((stringArr: string[]) => {
+        let tempStr = "";
         stringArr.forEach((str: string) => {
-          newLine += str;
+          tempStr += str + ", ";
         });
+        body.push(tempStr.substring(0, tempStr.length - 2));
+        tempStr = "";
+      });
+
+      body.forEach((str: string) => {
+        newCommands.push(<div>{str}</div>);
       });
     }
-    return newLine;
+
+    return <div>{newCommands}</div>;
   };
+
+  function handleMode() {
+    if (mode === "Brief") {
+      setMode("Verbose");
+    } else {
+      setMode("Brief");
+    }
+  }
 
   return (
     <div className="repl">
       {/*This is where your REPLHistory might go... You also may choose to add it within your REPLInput 
       component or somewhere else depending on your component organization. What are the pros and cons of each? */}
       {/* Update your REPLHistory and REPLInput to take in new shared state as props */}
-      <REPLHistory submittedCommands={submittedCommands} />
+      <div className="mode">
+        Mode:
+        <button className="mode" onClick={handleMode}>
+          {mode}
+        </button>
+      </div>
+      <REPLHistory
+        mode={mode}
+        submittedCommands={submittedCommands}
+        submittedInputs={submittedInputs}
+      />
       <hr></hr>
       <REPLInput addCommand={addCommand} />
     </div>
--- src/components/REPLHistory.tsx
+++ src/components/REPLHistory.tsx
@@ -2,13 +2,24 @@ import "../styles/main.css";
 
 interface REPLHistoryProps {
   // Fill with some shared state tracking all the pushed commands
-  submittedCommands: string[];
+  submittedCommands: JSX.Element[];
+  submittedInputs: string[];
+  mode: string;
 }
 export function REPLHistory(props: REPLHistoryProps) {
-  const { submittedCommands } = props;
+  const { submittedCommands, mode, submittedInputs } = props;
 
-  const hist = submittedCommands.map((value: string) => {
-    return <div key={value}>{value}</div>;
+  const hist = submittedCommands.map((value: JSX.Element) => {
+    if (mode === "Verbose") {
+      return (
+        <div key={value.key}>
+          <div>{submittedInputs}</div>{" "}
+          {/* TODO: fix to just be for the corresponding input*/}
+          <div>{value}</div>
+        </div>
+      );
+    }
+    return <div key={value.key}>{value}</div>;
   });
 
   return <div className="repl-history">{hist}</div>;

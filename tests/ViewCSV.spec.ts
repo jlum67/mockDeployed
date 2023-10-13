14c0e85e10bbf4205b490f11e6dd147afdbe7a4b
Tiffany Huang <goldentogawhale1258@gmail.com>
2023-10-13 10:21:59 -0400

    feat: testing view

--- tests/ViewCSV.spec.ts
+++ tests/ViewCSV.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { mockedJson} from mockedJson;
+import mockedJson from "../src/modules/mockedJson";
 
 /**
   The general shapes of tests in Playwright Test are:
@@ -28,8 +28,14 @@ test("on brief mode, I can load and view a file with headers", async ({
   const headers = mockedJson("file1").data.headers;
   const body = mockedJson("file1").data.body;
 
-  for (let i = 0; i < rowValues.length; i++) {
-    await expect(page.getByText(rowValues[i])).toBeVisible;
+  for (let i = 0; i < headers.length; i++) {
+    await expect(page.getByText(headers[i])).toBeVisible;
+  }
+
+  for (let j = 0; j < body.length; j++) {
+    body[j].forEach(async (cell: string) => {
+      await expect(page.getByText(cell)).toBeVisible;
+    });
   }
 });
 
@@ -37,7 +43,22 @@ test("on brief mode, I can load and view a file without headers", async ({
   page,
 }) => {
   await page.getByLabel("Command input").click();
-  await page.getByLabel("Command input").fill("load_file file1");
+  await page.getByLabel("Command input").fill("load_file file1noheaders");
   await page.getByRole("button", { name: "Submit" }).click();
   await expect(page.getByText("file1 successfully loaded!")).toBeVisible;
+  await page.getByLabel("Command input").click();
+  await page.getByLabel("Command input").fill("view");
+
+  const headers = mockedJson("file1noheaders").data.headers;
+  const body = mockedJson("file1noheaders").data.body;
+
+  for (let i = 0; i < headers.length; i++) {
+    await expect(page.getByText(headers[i])).toBeVisible;
+  }
+
+  for (let j = 0; j < body.length; j++) {
+    body[j].forEach(async (cell: string) => {
+      await expect(page.getByText(cell)).toBeVisible;
+    });
+  }
 });

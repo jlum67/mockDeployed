07a168e34e058d05aced260e826e033e67589be6
Tiffany Huang <goldentogawhale1258@gmail.com>
2023-10-13 13:27:12 -0400

    docs: App tests

--- tests/App.spec.ts
+++ tests/App.spec.ts
@@ -8,6 +8,10 @@ import { test, expect } from "@playwright/test";
   Look for this pattern in the tests below!
  */
 
+/**
+ * Test suite for the App
+ */
+
 // If you needed to do something before every test case...
 test.beforeEach(async ({ page }) => {
   // ... you'd put it here.
@@ -15,45 +19,35 @@ test.beforeEach(async ({ page }) => {
 });
 
 /**
- * Don't worry about the "async" yet. We'll cover it in more detail
- * for the next sprint. For now, just think about "await" as something
- * you put before parts of your test that might take time to run,
- * like any interaction with the page.
+ * Check that an input exists
  */
 test("on page load, i see an input bar", async ({ page }) => {
-  // Notice: http, not https! Our front-end is not set up for HTTPs.
-  await page.goto("http://localhost:8000/");
   await expect(page.getByLabel("Command input")).toBeVisible();
 });
 
+/**
+ * Check that the input box changes when you type in it
+ */
 test("after I type into the input box, its text changes", async ({ page }) => {
-  // Step 1: Navigate to a URL
-  await page.goto("http://localhost:8000/");
-
-  // Step 2: Interact with the page
-  // Locate the element you are looking for
   await page.getByLabel("Command input").click();
   await page.getByLabel("Command input").fill("Awesome command");
 
-  // Step 3: Assert something about the page
-  // Assertions are done by using the expect() function
   const mock_input = `Awesome command`;
   await expect(page.getByLabel("Command input")).toHaveValue(mock_input);
 });
 
 /**
- * Don't worry about the "async" yet. We'll cover it in more detail
- * for the next sprint. For now, just think about "await" as something
- * you put before parts of your test that might take time to run,
- * like any interaction with the page.
+ * Check that a header exists
  */
 test("on page load, i see a header", async ({ page }) => {
-  // Notice: http, not https! Our front-end is not set up for HTTPs.
   await expect(page.getByLabel("Command input")).toBeVisible();
   await expect(page.getByRole("heading")).toBeVisible();
   await expect(page.getByRole("heading")).toBeVisible();
 });
 
+/**
+ * Check that a Mode element exists and that it's set to "Brief"
+ */
 test("on page load, I see the mode is set to 'brief'", async ({ page }) => {
   await expect(page.getByLabel("Mode-Header")).toBeVisible;
   await expect(page.getByLabel("Mode")).toContainText(["Brief"]);
